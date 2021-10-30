dd0debd0e0c23a0314943ddf2d9c15e083300d30
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-10-30 13:22:29 +0530

    added error handling in upload form
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+ 
 
 const UploadForm = () => {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+
+    const allowedTypes = ['image/jpeg', 'image/png'];
+    const changeHandler = (e) => {
+        let selected = e.target.files[0]
+        console.log(selected);
+
+        if (selected && allowedTypes.includes(selected.type)){
+            setFile(selected);
+        }else {
+            setFile(null);
+            setError('Select a valid image file');
+        }
+    }
     return(
         <form>
-            <input type="file"/>
+            <input type="file" onChange={changeHandler}/>
         </form>
     )
 } 
