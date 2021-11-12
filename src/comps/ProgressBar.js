84bdf314fbb45aea18b5c37772847f9893199f40
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-11-12 12:53:39 +0530

    adding progress bar
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,11 @@
+import react from "react";
+import useStorage from "../hooks/useStorage";
+
+const ProgressBar = () => {
+
+    return(
+        <div className="ProgressBar">progress</div>
+    )
+}
+
+export default ProgressBar;
